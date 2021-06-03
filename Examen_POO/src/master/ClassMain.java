0a3c986af7051e7c7f91b7c1587431908f71b023
Xavier Degand <xavier.degand@hainaut-promsoc.be>
2021-06-03 14:55:37 +0200

    Change sysout ClassMain

--- Examen_POO/src/master/ClassMain.java
+++ Examen_POO/src/master/ClassMain.java
@@ -3,7 +3,7 @@ package master;
 public class ClassMain {
 
 	public static void main(String[] args) {
-		System.out.println("Have a good job !");
+		System.out.println("Have a good examen !");
 
 	}
 
