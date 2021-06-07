106253b455240f8371cd2e6f38e0fcdf53af75ec
Mika <Mika@MIKA-PC>
2021-06-07 18:08:16 +0200

    Creation Class Main

--- Examen_POO/src/master/ClassMain.java
+++ Examen_POO/src/master/ClassMain.java
@@ -3,7 +3,7 @@ package master;
 public class ClassMain {
 
 	public static void main(String[] args) {
-		System.out.println("Have a good day !!!");
+		System.out.println("Have a good day !");
 
 	}
 
