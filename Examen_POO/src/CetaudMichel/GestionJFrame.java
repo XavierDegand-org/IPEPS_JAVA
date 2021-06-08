3a45b3b48f54e65335a1595dfe268492e959eb5e
cetaud <contact@deyemon.be>
2021-06-08 13:49:00 +0200

    Affichage avec setFixedLenght. J'ai mis la date dans le nom du fichier.

--- Examen_POO/src/CetaudMichel/GestionJFrame.java
+++ Examen_POO/src/CetaudMichel/GestionJFrame.java
@@ -14,14 +14,14 @@ import java.util.Scanner;
 
 public class GestionJFrame extends JFrame {
 	
-	JButton btnLoad = new JButton("Chargement du personnel");
-	JButton btnAffichage = new JButton("Affichage liste du personnel");
-	JButton btnMag = new JButton("Création du magasin");
-	JButton btnPret = new JButton("Prêt de matériel");
-	JButton btnRetour = new JButton("Retour de matériel");
-	JButton btnPersonnel = new JButton("Modification données Personnel");
-	JButton btnSauvegarde = new JButton("Sauvegarde");
-	JButton btnClose = new JButton("Fermer");
+	private JButton btnLoad = new JButton("Chargement du personnel");
+	private JButton btnAffichage = new JButton("Affichage liste du personnel");
+	private JButton btnMag = new JButton("Création du magasin");
+	private JButton btnPret = new JButton("Prêt de matériel");
+	private JButton btnRetour = new JButton("Retour de matériel");
+	private JButton btnPersonnel = new JButton("Modification données Personnel");
+	private JButton btnSauvegarde = new JButton("Sauvegarde");
+	private JButton btnClose = new JButton("Fermer");
 	
 	static ArrayList<Personnel> personnel = new ArrayList<Personnel>();
 	
@@ -123,9 +123,25 @@ public class GestionJFrame extends JFrame {
 			System.out.println("|    Département     |    Prénom      |       nom      | Sex  |    Naissance   |          Email          |");
 			System.out.println("+--------------------+----------------+----------------+------+----------------+-------------------------+");
 		
+			StringBuilder tab = new StringBuilder();
+			
+			for(int i=0; i<personnel.size();i++) {
+				tab.append(setFixedLength(personnel.get(i).getDepartement().toString()));
+				tab.append(setFixedLength(personnel.get(i).getPrenom()));
+				tab.append(setFixedLength(personnel.get(i).getNom()));
+				tab.append(setFixedLength(personnel.get(i).getSexe().toString()));
+				tab.append(setFixedLength(personnel.get(i).getDateddMMyyyy()));
+				tab.append(setFixedLength(personnel.get(i).getEmail()));
+				tab.append("\n");
+			
+			}
+			
+			System.out.println(tab);
+			
+			/*
 			for(int i=0;i<personnel.size();i++) {
 				System.out.println(personnel.get(i));
-			}	
+			}	*/
 	}
 	
 	public void GestionPersonnel() {
@@ -140,11 +156,11 @@ public class GestionJFrame extends JFrame {
 			System.out.println("Introduire le nom de la personne à modifier :");
 			System.out.print("Quel est le nom ? : ");
 			
+			String input = InputData.InputData();
+			
 			for(int i=0;i<personnel.size();i++){
 				
-				if(personnel.get(i).getNom().equals(InputData.InputData())){
-					
-					System.out.println(personnel.get(i).toString());
+				if(personnel.get(i).getNom().equals(input)){
 					
 					System.out.println(i + 1 + "  " + personnel.get(i).getNom() + " -- " + personnel.get(i).getPrenom() + " -- " + personnel.get(i).getSexe() + " -- " + personnel.get(i).getEmail() + " -- " + personnel.get(i).getDepartement());
 					System.out.println("Introduire les nouvelles valeurs :");
@@ -176,15 +192,15 @@ public class GestionJFrame extends JFrame {
 			return;
 		}
 		
+		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss");
+		Date date = new Date();
+		
 		System.out.print("Introduire le nom du fichier ! : ");
 		String name = InputData.InputData();
 		
-		FileWriter writer = new FileWriter(System.getProperty("user.dir") + "/" + name + ".txt");
+		FileWriter writer = new FileWriter(System.getProperty("user.dir") + "/" + name + " " + format.format(date) + ".txt");
 		
-		SimpleDateFormat format = new SimpleDateFormat();
-		Date date = new Date();
-		
-		writer.write("DTC de la sauvegarde : " + format.format(date));
+		writer.write("DTG de la sauvegarde : " + format.format(date));
 		writer.write(System.getProperty( "line.separator" ));
 		writer.write("+--------------------+----------------+----------------+------+----------------+-------------------------+");
 		writer.write(System.getProperty( "line.separator" ));
@@ -203,6 +219,20 @@ public class GestionJFrame extends JFrame {
 		writer.close();
 		
 		System.out.println("Fichier '" + name + ".txt' a bien été créé ! ");
+	}
+	
+	
+	public String setFixedLength(String string) {
+		StringBuilder fullString = new StringBuilder(string);
+		
+		while (fullString.length()< 30) {
+			String d = " ";
+			char c1 = d.charAt(0);
+			fullString.insert(string.length(), c1);
+		}
+		//TODO size en fonction des colonnes
+		String c = fullString.substring(0,30);
+		return c;
 	}
 }
 
