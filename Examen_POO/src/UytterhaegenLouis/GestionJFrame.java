ad4559e97d338a9f7e9c08caad9ac0f0ef05a061
UytterhaegenLouis <louis.uytterhaegen@hainaut-promsoc.be>
2021-06-21 16:54:27 +0200

    Ajout de commentaires Ajout de la fonctionnalité prêt de matériel ( Fonctionnement ok ) Ajout de la fonctionnalité retour matériel ( Début uniquement) Ajout d'une fonction StringBuilder pour l'affichage des emprunts dans la sauvgarde Suppresion de certaines variables,... inutiles

--- Examen_POO/src/UytterhaegenLouis/GestionJFrame.java
+++ Examen_POO/src/UytterhaegenLouis/GestionJFrame.java
@@ -16,11 +16,9 @@ import java.util.ArrayList;
 
 public class GestionJFrame extends JFrame  {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	// Création des boutons et des textes des boutons
+	/* ******************************************************* 
+	 *****Création des boutons et leurs dénominations******* */
 	private JButton btnLoad = new JButton( "Chargement du personnel" );  
 	private JButton btnAffichage = new JButton( "Affichage liste du Personnel" );
 	private JButton btnMag = new JButton( "Création du magasin" );
@@ -31,12 +29,16 @@ public class GestionJFrame extends JFrame  {
 	private JButton btnClose = new JButton( "Fermer" );
 
 	public final static int tailleNom = 30;
-	private  ArrayList<Personnel> ListePersonnel = new ArrayList<Personnel>();
-	private Magasin magasin =  new Magasin(); ;
+	private  ArrayList<Personnel> ListePersonnel = new ArrayList<Personnel>(); // initialisation de la liste Personnel
+	private Magasin magasin =  new Magasin(); ; // initialisation du Magasin
+	private ArrayList<Emprunt> ListeEmprunt = new ArrayList<>(); // initialisation de la liste Emprunt
+
+
+
 
 
 	public GestionJFrame() {
-		super("Gestion d'une entreprise");
+		super("Gestion d'une entreprise"); // permet d'attribuer un titre à la fenêtre 
 		/* Mise en place de l'affichage des boutons et de la disposition*/
 		this.setDefaultCloseOperation( DISPOSE_ON_CLOSE ); // Permet de fermer le programme quand on ferme la fenêtre
 
@@ -53,6 +55,8 @@ public class GestionJFrame extends JFrame  {
 		btnSauvegarde.setBounds(1,202 ,200, 100);
 		btnClose.setBounds(201,202, 200, 100);
 
+		/* ******************************************************* 
+		 ********** Ajout des bouton sur l'affichage************ */
 
 		contentPane.add( btnLoad );
 		contentPane.add( btnAffichage );
@@ -66,26 +70,27 @@ public class GestionJFrame extends JFrame  {
 		this.setSize( 618 , 340 ); // Taille de l'écran de base
 		this.setLocationRelativeTo( null ); // centre la fenêtre
 
-		/* ******************************************************* */
-		/* Ajout des fonctions liés au boutons */
+		/* ******************************************************* 
+		 ********** Ajout des fonctions liés au boutons********* */
 		btnLoad.addActionListener(new LoadPersonnel());
 		btnAffichage.addActionListener(new Affichage());
 		btnMag.addActionListener(new LoadMagasin());
-		btnPret.addActionListener(new Emprunt());
+		btnPret.addActionListener(new LoadEmprunt());
 		btnRetour.addActionListener(new RetourEmprunt());
 		btnPersonnel.addActionListener(new GestionPersonnel());
 		btnSauvegarde.addActionListener(new Sauvegarde());
 		btnClose.addActionListener(new Close());
 
 	}
-	
-	/* ******************************************************************
-	 *******************Bouton Chargement Personnel***********************/								
 
-	private class LoadPersonnel implements ActionListener{ // Mise en place d'une classe interne 
+	/* *******************************************************************
+	 *******************Bouton Chargement Personnel********************* */								
 
-		@Override
+	private class LoadPersonnel implements ActionListener{ // Mise en place d'une classe interne qui possède les fonctions implémenter par la classe ActionListener
+
+		@Override // Réécrit une fonction qui existe déjà en la réimplémentant comme on le souhaite
 		public void actionPerformed (ActionEvent e) {
+			// Ajout des différents membres du personnel Si  la liste est vide au départ
 			if (ListePersonnel.isEmpty()) {
 				ListePersonnel.add(new Personnel("Collon","Albert",Sexe.HOMME, new MyDate(10, 8, 1990), "Collon.a@test.be",Departement.HR));
 				ListePersonnel.add(new Personnel("Peeters","Marie",Sexe.FEMME, new MyDate(01, 5, 1985), "Peeters_M@test.be", Departement.HR));	
@@ -101,82 +106,86 @@ public class GestionJFrame extends JFrame  {
 				ListePersonnel.add(new Personnel("Herman","Nathalie",Sexe.FEMME, new MyDate(26, 7, 2001), "herman.n@test,be",Departement.Prod));
 				ListePersonnel.add(new Personnel("Bontemps","Annie",Sexe.FEMME, new MyDate(23,9,1998), "bontemps.a@test.be",Departement.Prod));
 			}
-			else {
-				System.out.println("Magasin déjà chargé !");
+			else { // Si on a déjà chargé la liste auparavant
+				System.out.println("Personnel déjà chargé !");
 			}
 		}
 	}
 
-	/* ******************************************************************
-	 ********************Bouton Affichage Personnel***********************/
+	/* *******************************************************************
+	 ********************Bouton Affichage Personnel********************* */
 
-	private  StringBuilder  Affichage() {
-		StringBuilder SB = new StringBuilder();
-		SB.append("+---------------------+------------------------------+------------------------------+-------+------------+---------------------------+\n");
-		SB.append("| Département         | Prénom                       | Nom                          | Sexe  | Naissance  | Email                     |\n");
-		SB.append("+---------------------+------------------------------+------------------------------+-------+------------+---------------------------+\n");
-		for (Personnel personne : ListePersonnel) {
-			SB.append("  ");
-			SB.append(setFixedLength(personne.getDepartement(),20) + " ");
-			SB.append(setFixedLength(personne.getPrenom(),30)+ " ");
-			SB.append(setFixedLength(personne.getNom(),30)+ " ");
-			SB.append(setFixedLength(personne.getSexe(),7)+ " ");
-			SB.append(setFixedLength(personne.getDateddMMyyyy(),12)+ " ");
-			SB.append(setFixedLength(personne.getEmail(),30)+ " ");
-			SB.append("\n");
-		}	
+	private  StringBuilder  Affichage() { // Fonction permettant d'afficher tout le personnel sous forme d'un tableau 
+
+		StringBuilder SB = new StringBuilder(); // permet de concaténer des chaînes de caractères pour que cela soit plus propre et permet de tout afficher
+
+		if (ListePersonnel.isEmpty()) { // si la liste du personnel est vide
+			return SB.append("Affichage impossible, pas de personnel !");
+		}
+		else { // si la liste du personnel n'est pas vide
+
+			SB.append("+---------------------+------------------------------+------------------------------+-------+------------+---------------------------+\n");
+			SB.append("| Département         | Prénom                       | Nom                          | Sexe  | Naissance  | Email                     |\n");
+			SB.append("+---------------------+------------------------------+------------------------------+-------+------------+---------------------------+\n");
+			for (Personnel personne : ListePersonnel) { // Boucle pour afficher les membres du personnel avec leurs attributs / Type objet   objet courant  : Arraylist 
+				SB.append("  ");
+				SB.append(setFixedLength(personne.getDepartement(),20) + " ");
+				SB.append(setFixedLength(personne.getPrenom(),30)+ " ");
+				SB.append(setFixedLength(personne.getNom(),30)+ " ");
+				SB.append(setFixedLength(personne.getSexe(),7)+ " ");
+				SB.append(setFixedLength(personne.getDateddMMyyyy(),12)+ " ");
+				SB.append(setFixedLength(personne.getEmail(),30)+ " ");
+				SB.append("\n");
+			}	
+			return SB;
+		}
 
 
-
-		return SB;
 	}
-	
-	private class Affichage implements ActionListener{
+
+	private class Affichage implements ActionListener{ // Mise en place d'une classe interne pour l'affichage qui possède les fonctions implémenter par la classe ActionListener
 
 		@Override
-		public void actionPerformed (ActionEvent e) {
-			if (ListePersonnel.isEmpty()) {
-				System.out.println("Affichage impossible, pas de personnel !");
-			}
-			else {
-				StringBuilder texteAAfficher = Affichage();
-				System.out.println(texteAAfficher.toString());
-			}
+		public void actionPerformed (ActionEvent e) { // Lorsque l'on appuie sur le bouton Affichage 
+
+			System.out.println(Affichage().toString());
+
 
 		}
 	}
 
 	/* ******************************************************************
 	 **************Permet de calibrer la taille des Strings***************/
-	private String setFixedLength(String s, int taille) {
+	private String setFixedLength(String s, int taille) { // fonction permettant de mettre une taille fixe pour un String
 		StringBuilder stringtest= new StringBuilder(s);
 		while (stringtest.length()< taille) { 	// Rajouter des espaces jusqu'à ce qu'on atteigne le nombre de caractères demandés pour l'affichage 									
 			stringtest.insert(s.length(),' ');
 		}
-		return stringtest.substring(0,taille);
+		return stringtest.substring(0,taille); // Permet de renvoyer la chaîne avec le nombre d'espace en fonction de la taille demandée
 
 	}	
 
-	/* ******************************************************************
-	 ***********************Bouton Création Magasin***********************/
+	/* *******************************************************************
+	 ***********************Bouton Création Magasin********************* */
 
 
-	private class LoadMagasin implements ActionListener{
+	private class LoadMagasin implements ActionListener{ // Mise en place d'une classe interne  pour la création du magasin qui possède les fonctions implémenter par la classe ActionListener
 
 		@Override
-		public void actionPerformed (ActionEvent e) {
-			
-			if (magasin.isEmpty()) {
+		public void actionPerformed (ActionEvent e) {  // Lorsque l'on appuie sur le bouton création du magasin
+
+			if (magasin.isEmpty()) {	// Si le magasin est vide
 				magasin.ajouterProduit("HP1", "Elitebook 850 G7");
 				magasin.ajouterProduit("HP2", "Elitebook 830 G7 X360");
 				magasin.ajouterProduit("Dell1", "Inspiron 15 3000");
 				magasin.ajouterProduit("Dell2", "XPS 13");
 				magasin.ajouterProduit("Dell3", "XPS 15");
-				magasin.ajouterProduit("Dell", "Thinkpad E15 G2");
-				magasin.ajouterProduit("Dell", "IdeaPad 3 14IIL05 81WD00B2MH");
+				magasin.ajouterProduit("Lenovo", "Thinkpad E15 G2");
+				magasin.ajouterProduit("Lenovo", "IdeaPad 3 14IIL05 81WD00B2MH");
 				magasin.listeMap();
+				System.out.println("\n");
 			}
-			else {
+			else {	// Si le magasin a déjà été créé
 				System.out.println("Magasin déjà chargé !");
 			}
 
@@ -185,26 +194,95 @@ public class GestionJFrame extends JFrame  {
 
 
 
-	/* ******************************************************************
-	 ***********************Bouton Prêt du Matériel***********************/
+	/* *******************************************************************
+	 ***********************Bouton Prêt du Matériel********************* */
 
-	private class Emprunt implements ActionListener{
+	private void Emprunt () { // Fonction permettant l'affichage des différents emprunts
+		System.out.println("   Prêt de Matériel   ");
+		System.out.println("----------------------");
+		for (int CompteurList = 0; CompteurList < ListeEmprunt.size(); CompteurList++) {
+			System.out.println(ListeEmprunt.get(CompteurList).toString());
+		}
+		System.out.println("\n");
+	}
+
+	private class LoadEmprunt implements ActionListener{  // Lorsque l'on appuie sur le bouton prêt matériel
 
 		@Override
 		public void actionPerformed (ActionEvent e) {
+			if (magasin.isEmpty()) {
+				System.out.println("Le magasin n'a pas encore été chargé. Veuillez le charger d'abord !");
+			}
+			else if(ListePersonnel.isEmpty()){
+				System.out.println("Le personnel n'a pas encore été chargé. Veuillez le charger d'abord !");
+			}
+			else if (ListeEmprunt.isEmpty()) {
+
+				ListeEmprunt.add(new Emprunt(ListePersonnel.get(0),magasin.listeProduit.get(1)));
+				ListeEmprunt.add(new Emprunt(ListePersonnel.get(1),magasin.listeProduit.get(3)));
+				ListeEmprunt.add(new Emprunt(ListePersonnel.get(2),magasin.listeProduit.get(4)));
+				ListeEmprunt.add(new Emprunt(ListePersonnel.get(3),magasin.listeProduit.get(6)));
+				ListeEmprunt.add(new Emprunt(ListePersonnel.get(6),magasin.listeProduit.get(7)));
+				Emprunt();
+
+			}
+			else
+			{
+				Emprunt();	
+			}
 
 		}
 	}
 
-	/* ******************************************************************
-	 ********************Bouton Retour Emprunt****************************/
+	/* *******************************************************************
+	 ********************Bouton Retour Emprunt************************** */
 
 	private class RetourEmprunt implements ActionListener{
 
 		@Override
 		public void actionPerformed (ActionEvent e) {
+			if (magasin.isEmpty()) {
+				System.out.println("Le magasin n'a pas encore été chargé. Veuillez le charger d'abord !");
+			}
+			else if(ListePersonnel.isEmpty()){
+				System.out.println("Le personnel n'a pas encore été chargé. Veuillez le charger d'abord !");
+			}
+			else if(ListeEmprunt.isEmpty()) {
+				System.out.println("Aucun prêt n'a été chargé !");
+			}
+			else {
+				System.out.println("   Liste des emprunts   ");
+				System.out.println("------------------------");
+				for (int CompteurList = 0 ; CompteurList < ListeEmprunt.size(); CompteurList++) {
+					Emprunt emprunteur = ListeEmprunt.get(CompteurList);
+					System.out.println("N° "+emprunteur.getNombre()+" "+emprunteur.getEmprunteur()+" "+emprunteur.getMateriel()+" "+emprunteur.getArticle());
+				}
+				System.out.println("Introduire le numéro d'emprunt à annuler");
+				int InputEmpruntToCancel = Lire.nbre();
 
+
+			}
 		}
+	}
+
+	/* ******************************************************************
+	 ********Fonction Affichage Emprunt pour la sauvegarde************* */
+
+	private StringBuilder AffichageEmprunt () {
+		StringBuilder SB = new StringBuilder(); 
+		SB.append("+------+------------------------------------------------------------+----------------------------------------------+\n");
+		SB.append("| N°   | Nom-Prénom                                                 | Matériel                                     |\n");
+		SB.append("+------+------------------------------------------------------------+----------------------------------------------+\n");
+		for (int CompteurList = 0 ; CompteurList < ListeEmprunt.size(); CompteurList++) { // Boucle pour afficher les différents emprunts
+			Emprunt emprunteur = ListeEmprunt.get(CompteurList); // création d'un objet de type Personnel pour récupérer les informations dans la liste
+			SB.append("  ");
+			SB.append(emprunteur.getNombre() + "      ");
+			SB.append(setFixedLength(emprunteur.getEmprunteur(),60)+ " ");
+			SB.append(setFixedLength(emprunteur.getMateriel(),8 )+ " ");
+			SB.append(setFixedLength(emprunteur.getArticle(),30)+ " ");
+			SB.append("\n");
+		}	
+		return SB;	
 	}
 
 	/* ******************************************************************
@@ -221,25 +299,26 @@ public class GestionJFrame extends JFrame  {
 			}
 			else
 			{
-				Affichage();
+				System.out.println(Affichage().toString());
 				System.out.println("Introduire le nom de la personne à modifier :");
 				// On demande le nom de la personne à modifier
 				String s = InputData.inputNomPrenom("Nom");
 				Boolean loop = false;
 				int indice = 0;
-				while (!loop) {
+				while (!loop) { // Boucle pour trouver dans la boucle si le nom se trouve dans la liste et à quel indice
 					for (int CompteurList=0;CompteurList<ListePersonnel.size();CompteurList++){
-						if (ListePersonnel.get(CompteurList).getNom().equals(s)){
+						if (ListePersonnel.get(CompteurList).getNom().equals(s)){ // Si le nom à l'indice X correspond au nom introduit 
 							loop = true;
 							indice = CompteurList;
 						}
 					}
-					if (!loop) {
+					if (!loop) { // Si le nom introduit n'existe pas dans la liste
 						System.out.println("Il n'existe personne ayant ce nom. Veuillez entrer un nom correct existant");
 						s = InputData.inputNomPrenom("Nom");
 					}
 				}
-				Personnel personne = ListePersonnel.get(indice);
+				Personnel personne = ListePersonnel.get(indice); // création d'un objet de type Personnel pour récupérer les informations dans la liste
+				// On affiche L'iD du personne, le nom, ... de la personne dont on souhaite changer les informations
 				System.out.println(personne.getIdPersonnel()+"   "+personne.getNom()+" -- "+personne.getPrenom()+" -- "+personne.getSexe()+" -- "+personne.getEmail()+" -- "+personne.getDepartement());
 				System.out.println("Introduire les nouvelles valeurs :");
 				// On Demande le Nom
@@ -248,8 +327,8 @@ public class GestionJFrame extends JFrame  {
 				personne.setPrenom(InputData.inputNomPrenom("Prénom"));
 				// On Demande l'adresse mail
 				personne.setEmail(InputData.inputEmail());
-				System.out.println("----------------------");
-				System.out.println("Modifications validées");
+				System.out.println("--------------------------");
+				System.out.println("  Modifications validées  ");
 			}
 		}
 	}
@@ -263,13 +342,27 @@ public class GestionJFrame extends JFrame  {
 
 		@Override
 		public void actionPerformed (ActionEvent e) {
-			try (BufferedWriter bufWrite = new BufferedWriter (new FileWriter(new File("./src/UytterhaegenLouis","TEST.txt"))))
-			{
-				bufWrite.write(Affichage().toString());
-				bufWrite.newLine();
+			if (magasin.isEmpty()) {
+				System.out.println("Le personnel n'a pas encore été chargé. Veuillez le charger d'abord !");
+			}
+			else if(ListePersonnel.isEmpty()){
+				System.out.println("Le magasin n'a pas encore été chargé. Veuillez le charger d'abord !");
+			}
+			else if(ListeEmprunt.isEmpty()) {
+				System.out.println("Aucun prêt n'a été chargé !");
+			}
+			else {
+				try (BufferedWriter bufWrite = new BufferedWriter (new FileWriter(new File("./src/UytterhaegenLouis","TEST.txt"))))
+				{
+					bufWrite.write(Affichage().toString());
+					bufWrite.newLine();
+					bufWrite.newLine();
+					bufWrite.write(AffichageEmprunt().toString());
+					bufWrite.newLine();
 
-			} catch (IOException io) {
-				System.err.println("Une erreur est survenue : "+io.getMessage());
+				} catch (IOException io) {
+					System.err.println("Une erreur est survenue : "+io.getMessage());
+				}
 			}
 		}
 	}
