482689c8f28d046890790ac48379876f5cd4e6df
UytterhaegenLouis <louis.uytterhaegen@hainaut-promsoc.be>
2021-06-21 19:13:36 +0200

    Modification du Regex pour l'email

--- Examen_POO/src/UytterhaegenLouis/ControleSaisie.java
+++ Examen_POO/src/UytterhaegenLouis/ControleSaisie.java
@@ -30,7 +30,7 @@ public class ControleSaisie {
 	
 
 	private static final String EMAIL_PATTERN
-	         = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";	         
+	         = "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";	         
 	
 	private static Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 	
