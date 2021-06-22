a0dfad2bc53387dcbe79fad8bdcde994211e0d3b
UytterhaegenLouis <louis.uytterhaegen@hainaut-promsoc.be>
2021-06-22 17:18:47 +0200

    Correction Indentation

--- Examen_POO/src/UytterhaegenLouis/Sexe.java
+++ Examen_POO/src/UytterhaegenLouis/Sexe.java
@@ -1,15 +1,15 @@
 package UytterhaegenLouis;
 
 public enum Sexe {
-	 HOMME("Garçon"),
-	 FEMME("Fille"),;
-	 private final String label;
-	
-	 Sexe(String label) {
-	        this.label = label;
-	 }
+	HOMME("Garçon"),
+	FEMME("Fille"),;
+	private final String label;
 
-	 public String getLabel() {
-	        return label;
-	 }
+	Sexe(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
 }
