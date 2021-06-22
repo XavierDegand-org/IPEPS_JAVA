0c36c748e114dc887fec51f4d691e453dad94b70
UytterhaegenLouis <louis.uytterhaegen@hainaut-promsoc.be>
2021-06-22 17:10:55 +0200

    Changement indentation

--- Examen_POO/src/UytterhaegenLouis/ClassMain.java
+++ Examen_POO/src/UytterhaegenLouis/ClassMain.java
@@ -4,14 +4,14 @@ import javax.swing.UIManager;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 public class ClassMain {
-	
-	
+
+
 
 	public static void main(String[] args) throws Exception {
-		
+
 		UIManager.setLookAndFeel(new NimbusLookAndFeel());
-		
-		
+
+
 		// Démarre la présentation
 		GestionJFrame Gestion = new  GestionJFrame();
 		Gestion.setVisible(true);
