587435c8b8ed8ff50ba690c20405e34c75b0de83
UytterhaegenLouis <louis.uytterhaegen@hainaut-promsoc.be>
2021-06-22 17:11:50 +0200

    Changement Regex Email + Ajout fonction valideNomFichier()

--- Examen_POO/src/UytterhaegenLouis/ControleSaisie.java
+++ Examen_POO/src/UytterhaegenLouis/ControleSaisie.java
@@ -4,18 +4,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ControleSaisie {
-	
-	// private static Matcher matcher;
-	
-	 public static boolean valideNom(String text,int taille) {
-		 String nullString = null;
-		 String vide = new String();
-		 boolean estNull = text.equals(nullString);
-		 boolean estVide = text.equals(vide);
-		 if(estNull || estVide){
+
+	private static final String EMAIL_PATTERN
+	= "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";	         
+
+	private static Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+
+	public static boolean valideNom(String text,int taille) {
+		String nullString = null;
+		String vide = new String();
+		boolean estNull = text.equals(nullString);
+		boolean estVide = text.equals(vide);
+		if(estNull || estVide){
 			System.out.println("Value is null or empty !");
-			 return false;
-		 } 
+			return false;
+		} 
 		else{
 			if (text.matches("^([ \\u00c0-\\u01ffa-zA-Z'\\-])+$") && text.length() <= taille) { 
 				return true;
@@ -25,19 +28,35 @@ public class ControleSaisie {
 			}
 		}
 		return false;
-	       
-	 }
-	
 
-	private static final String EMAIL_PATTERN
-	         = "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";	         
+	}
 	
-	private static Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-	
+	public static boolean valideNomFichier(String text,int taille) {
+		String nullString = null;
+		String vide = new String();
+		boolean estNull = text.equals(nullString);
+		boolean estVide = text.equals(vide);
+		if(estNull || estVide){
+			System.out.println("Value is null or empty !");
+			return false;
+		} 
+		else{
+			if (text.matches("^[a-zA-Z_0-9]{1,}+(.txt)$") && text.length() <= taille) { 
+				return true;
+			}
+			else {
+				System.out.println("Nom de fichier invalide !");
+				
+			}
+		}
+		return false;
+
+	}
+
 	public static boolean valideEmail( String hex) {
-	
-	     Matcher matcher = pattern.matcher(hex);
-	     return matcher.matches();
+
+		Matcher matcher = pattern.matcher(hex);
+		return matcher.matches();
 	}
 
 }
