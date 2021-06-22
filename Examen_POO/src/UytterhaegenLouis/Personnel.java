2b2c6ab46cdd97260b58751dfa09604cc15f0996
UytterhaegenLouis <louis.uytterhaegen@hainaut-promsoc.be>
2021-06-22 17:17:58 +0200

    Ajout du EmailPattern et de conditions dans le setEmail

--- Examen_POO/src/UytterhaegenLouis/Personnel.java
+++ Examen_POO/src/UytterhaegenLouis/Personnel.java
@@ -1,48 +1,60 @@
 package UytterhaegenLouis;
 
+import java.util.regex.Pattern;
+
 public class Personnel extends Individu {
 	private int IdPersonnel;
-	private String Email;
+	private String email;
 	private static int id=1;
 	private Departement Departement;
-	
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile( 
+    		"^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$" 
+    );
 
 	public Personnel(String nom, String prenom, Sexe sexe, MyDate dateNaissance, String Email, Departement Departement) {
 		super(nom, prenom, sexe, dateNaissance);
 		this.IdPersonnel = id++;
 		if ( ControleSaisie.valideEmail(Email)) { // Permet qu'en cas de mauvaise écriture de l'email, d'automatiquement écrire le mail dans un bon format
-			this.Email = Email;
-			} else {
-			this.Email = nom+"."+prenom.charAt(0) +"@test.be";
-					}
+			this.email = Email;
+		} else {
+			this.email = nom+"."+prenom.charAt(0) +"@test.be";
+		}
 		this.Departement= Departement;
-		}
-		
+	}
+
+
+	public int getIdPersonnel() {
+		return IdPersonnel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		if ( email == null ) {
+            throw new NullPointerException( "email cannot be null" );
+        }
+        if ( !EMAIL_PATTERN.matcher( email ).matches() ) {
+            throw new RuntimeException( "Le format de l'email ne coorespond pas aux patterns." );
+        }
+        this.email = email;
+    }
 	
-	 public int getIdPersonnel() {
-			return IdPersonnel;
-	  }
+	public String getDepartement() {
+		return Departement.getDeptNom();
+	}
+	@Override
+	public String toString() {
+		return String.format( "%s %s %s %s %s %s",
+				this.getDepartement(),
+				this.getPrenom(),
+				this.getNom(),
+				this.getSexe(),
+				this.getDateNaissance(),
+				this.email );
+	}
 
-	  public String getEmail() {
-			return Email;
-	  }
-
-	  public void setEmail(String Email) {
-			this.Email = Email;
-	  }
-	  public String getDepartement() {
-			return Departement.getDeptNom();
-	  }
-	  @Override
-		public String toString() {
-		  return String.format( "%s %s %s %s %s %s",
-	                this.getDepartement(),
-	                this.getPrenom(),
-	                this.getNom(),
-	                this.getSexe(),
-	                this.getDateNaissance(),
-	                this.Email );
-		}
-	  
 
 }
