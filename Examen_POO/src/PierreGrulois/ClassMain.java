6fb8aeff13071f075b9602f0b61565efb8cc947d
Pierre GRULOIS <pierre.grulois@hainaut-promsoc.be>
2021-06-27 15:07:21 +0200

    Exception levé

--- Examen_POO/src/PierreGrulois/ClassMain.java
+++ Examen_POO/src/PierreGrulois/ClassMain.java
@@ -1,9 +1,22 @@
-package master;
+package PierreGrulois;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 public class ClassMain {
 
-	public static void main(String[] args) {
-		System.out.println("Have a good dag !");
+	public static void main(String[] args) throws UnsupportedLookAndFeelException {
+
+		try {
+			UIManager.setLookAndFeel ( new NimbusLookAndFeel());
+		}catch(UnsupportedLookAndFeelException e) {
+			System.out.println("newLookAndFeel est non nul et newLookAndFeel.isSupportedLookAndFeel() renvoie false");
+		}
+		
+		// Start the demo
+		GestionJFrame demo = new GestionJFrame();
+		demo.setVisible(true);
 
 	}
 
