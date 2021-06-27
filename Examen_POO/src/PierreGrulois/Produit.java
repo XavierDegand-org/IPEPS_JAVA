1d378fefaf98e8d5ba4a22761dd0846e7751fe69
Pierre GRULOIS <pierre.grulois@hainaut-promsoc.be>
2021-06-27 14:48:44 +0200

    implementation des exceptions

--- Examen_POO/src/PierreGrulois/Produit.java
+++ Examen_POO/src/PierreGrulois/Produit.java
@@ -2,13 +2,43 @@ package PierreGrulois;
 
 public class Produit {
 	
+	private String nom;
+	private String description;
 	
-	
-	public Produit() {
-		
+	public Produit (String nom, String description) {
+		this.nom = nom;
+		this.description= description;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 	
+	@Override public String toString() {;
 	
+		try{
+			return String.format( " Produit [nom = %s, description = %s  ] \n",
+
+       
+				this.getNom(),
+				this.getDescription() );
+        
+		}catch(java.util.IllegalFormatException e) {
+			return "Une ou plusieurs entrées ne sont pas valide.";
+		} 
+	}
 	
 	
 
